3180d97bb0ef6ee50bf13560e62f0ad8f20575fa
Abigail Rick <abigail_rick@mailfence.com>
2024-09-18 08:50:10 -0500

    Update ChromeExtensionLead.js

--- ChromeExtensionLead.js
+++ ChromeExtensionLead.js
@@ -17,7 +17,7 @@ chrome.tabs.query({active: true, currentWindow: true,}, function(tabs){
 myLeads.push(tabs[0].url);
 localStorage.setItem("myLeads", JSON.stringify(myLeads) );
 render(myLeads);
-});
+})
 });
 
 
